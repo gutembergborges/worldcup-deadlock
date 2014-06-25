d6a0d634a4a59d1a1becc5df8bffce8e63e1791e
Tayane Cerqueira <engcomp.tayane@gmail.com>
2014-06-24 21:30:19 -0300

    Nova classe (teste)

---
+++ workspace/worldcup-deadlock/src/model/Jogo.java
@@ -0,0 +1,25 @@
+package model;
+
+public class Jogo {
+	
+	String data;
+	String local;
+	
+	public Jogo(){
+		
+	}
+	
+	public String getData() {
+		return data;
+	}
+	public void setData(String data) {
+		this.data = data;
+	}
+	public String getLocal() {
+		return local;
+	}
+	public void setLocal(String local) {
+		this.local = local;
+	}
+	
+}
